6927832cfe45febe341a88d2ef0c096cd3ed580d
Daphne Balane <balanedaphne20@gmail.com>
2023-08-17 20:43:11 +0800

    feat: added turnLeft, turnRight, setDirection methods and passed the should_change_to_location_0_0_W_when_executeCommand_given_0_0_North_and_command_TurnLeft test case

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,5 +1,7 @@
 package com.afs.tdd;
 
+import static java.nio.file.Files.move;
+
 public class MarsRover {
 
     private Location location;
@@ -13,6 +15,45 @@ public class MarsRover {
             if(location.getDirection() == Direction.NORTH){
                 location.setY(location.getY() + 1);
             }
+        } else if (givenCommand == Command.TURN_LEFT) {
+            turnLeft();
+        } else if (givenCommand == Command.TURN_RIGHT) {
+            turnRight();
+        }
+    }
+
+
+    private void turnRight() {
+        switch (location.getDirection()) {
+            case NORTH:
+                location.setDirection(Direction.EAST);
+                break;
+            case EAST:
+                location.setDirection(Direction.SOUTH);
+                break;
+            case SOUTH:
+                location.setDirection(Direction.WEST);
+                break;
+            case WEST:
+                location.setDirection(Direction.NORTH);
+                break;
+        }
+    }
+
+    private void turnLeft() {
+        switch (location.getDirection()) {
+            case NORTH:
+                location.setDirection(Direction.WEST);
+                break;
+            case WEST:
+                location.setDirection(Direction.SOUTH);
+                break;
+            case SOUTH:
+                location.setDirection(Direction.EAST);
+                break;
+            case EAST:
+                location.setDirection(Direction.NORTH);
+                break;
         }
     }
 
